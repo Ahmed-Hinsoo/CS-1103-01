36920d10207f29081d8a7788d57c4543294a7e84
Ahmed Ali <105525838+Ahmed-Hinsoo@users.noreply.github.com>
2026-04-22 22:11:05 +0200

    Create Product.java

---
+++ com/ecommerce/Product.java
@@ -0,0 +1,30 @@
+package com.ecommerce;
+
+public class Product {
+    private int productID;
+    private String name;
+    private double price;
+
+    public Product(int productID, String name, double price) {
+        this.productID = productID;
+        this.name = name;
+        if(price < 0) {
+            this.price = 0;
+        } else {
+            this.price = price;
+        }
+    }
+
+    public int getProductID() { return productID; }
+    public void setProductID(int productID) { this.productID = productID; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public double getPrice() { return price; }
+    public void setPrice(double price) {
+        if(price >= 0) this.price = price;
+    }
+    
+    public String getInfo() {
+        return name + " - $" + price;
+    }
+}
